dbab2376d352ac3c589fff79f4a0ad7ac2074680
Kumpol Tachatamrong <u6221016@au.edu>
2023-07-29 00:10:54 +0700

    Assignment

---
+++ Desktop/my-nuxt3-project/pages/beelzebub.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h2>Barbatos</h2>
+
+        <div class="image">
+            <img :src="'beelzebub.png'">
+        </div>
+
+        <p>
+            Raiden Ei (Japanese: 雷電影 Raiden Ei), also known by her Goetic name Beelzebub, is the God of Eternity and the current Electro Archon who presides over Inazuma. She is a member of The Seven who also currently functions as the Raiden Shogun of Inazuma. 
+        </p>
+
+        </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .image {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+    }
+
+    h2 {
+        margin-bottom:  20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+
+</style>
---
+++ Desktop/my-nuxt3-project/pages/buer.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h2>Barbatos</h2>
+
+        <div class="image">
+            <img :src="'buer.png'">
+        </div>
+
+        <p>
+            Lesser Lord Kusanali, also known by her Goetic name Buer, is the God of Wisdom and the current Dendro Archon among The Seven who presides over Sumeru and the new incarnation of the Dendro Archon, created by Greater Lord Rukkhadevata from the purest branch of Irminsul to succeed her. She currently resides in her vessel as Nahida. 
+        </p>
+
+        </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .image {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+    }
+
+    h2 {
+        margin-bottom:  20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+
+</style>
